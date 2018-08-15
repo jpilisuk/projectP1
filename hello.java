5a56ea91f6eeae6486f6b402de19b371a4cc77a4
Josh <jpilisuk@gmail.com>
2018-08-15 10:27:38 -0700

    changed file to just print hello world

--- hello.java
+++ hello.java
@@ -10,64 +10,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("------------------------");		//
-		System.out.println("using modulo");					//  this code is easier but is less efficient
-		System.out.println("------------------------");		//
 		
-		long starttime = System.nanoTime();
-		
-		int sum=0;
-		int j=0;
-		while(j<1000) {
-			if (j%3 == 0 || j%5 == 0) {
-				sum+=j;
-			}
-			j++;
-		}
-		
-		long endtime = System.nanoTime();
-		long totaltime = endtime-starttime;
-		
-		System.out.println("answer is: " + sum);
-		System.out.println("answer took: " + totaltime + "ns");
-
-		System.out.println("------------------------");		//
-		System.out.println("without using modulo");			// This code is a little longer but runs 3x as fast
-		System.out.println("------------------------");		//
-		
-		starttime = System.nanoTime();
-		
-		int newsum=0;
-		int i=0;
-		
-		while(i<1000)
-		{
-			newsum +=i;
-			i+=3;
-		}
-		
-		i=0;
-		
-		while(i<1000)
-		{
-			newsum +=i;
-			i+=5;
-		}
-		
-		i=0;
-		
-		while(i<1000)
-		{
-			newsum -=i;
-			i+=15;
-		}
-		
-		
-		endtime = System.nanoTime();
-		totaltime = endtime-starttime;
-		
-		System.out.println("answer is: " + newsum);
-		System.out.println("answer took: " + totaltime + "ns");		
+		System.out.println("Hello, World!");	
 		
 	}
 
